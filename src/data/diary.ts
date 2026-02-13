e358e81759b1408743cd9c851b07f4231ca42893
Ankush54920 <ankushsharma54920@gmail.com>
2026-02-13 23:16:28 +0530

    diary.ts

--- src/data/diary.ts
+++ src/data/diary.ts
@@ -11,7 +11,7 @@ export const diaryEntries: DiaryEntry[] = [
   { id: 2, title: "Page Two — Care for each other", paragraph: "There are nights when the silence is so loud, I swear I can hear your heartbeat from across the ocean. I hold my pillow and pretend it's you.",imageUrl: "/images/image2.jpg" },
   { id: 3, title: "Page Three — You", paragraph: `Some of the 1st Phots of yours that I clicked.
 Then some photos of You Eating Together with me, that Didn't felt like Dream ( But Definitely Dream come true)`,imageUrl: "/images/image3.jpg" },
-  { id: 4, title: "Page Four — Us( You and Me)", paragraph: `Its Me and Mari Bandi, Dekh lg rahi hai na, Jase Apsara Ya Pari 😌.`, imageUrl: "/images/image4.jpg" },
-  { id: 5, title: "Page Five — The Hard Truth", paragraph: ".  ", imageUrl: "/images/image5.jpg" },
+  { id: 4, title: "Page Four — Us( You & Me)", paragraph: `Its Me and Mari Bandi, Dekh lg rahi hai na, Jase Apsara Ya Pari 😌.`, imageUrl: "/images/image4.jpg" },
+  { id: 5, title: "Page Five — Me & You - You & Me", paragraph: "  ", imageUrl: "/images/image5.jpg" },
   { id: 6, title: "Page Six — Tu hi Tu 🫣", paragraph: `  . `,imageUrl: "/images/image6.jpg" },
 ];
