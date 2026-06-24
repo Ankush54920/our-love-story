76fe423952f91facd27f100b299a83ff68ea7093
Ankush54920 <ankushsharma54920@gmail.com>
2026-06-24 16:30:32 +0530

    Update memories.ts

--- src/data/memories.ts
+++ src/data/memories.ts
@@ -7,64 +7,65 @@ export interface Memory {
 }
 
 export const memories: Memory[] = [
-  { id: 1, title: "26th July:- The Day We First Talked", paragraph: "A chance encounter that changed everything. The universe conspired to bring us together in the most unexpected way." },
-  { id: 2, title: "30th July:- First Conversation", paragraph: "You asked, 'Wo tumhi the na jo App ka puch rahe the' Sentence that changed the future. We then talked regarding the app feedback until the teacher Appeared." },
-  { id: 3, title: "13th August:- First Walk", paragraph: "I was extremely nervous. I waited until you arrived. I asked. You agreed. And somehow… we started walking together,By the way Do you remember when Maharashtra was formed 🤔??"},
-  { id: 4, title: "14th August:- First Temple Visit and Eating together.",
+{ id: 1, title: "Ooye Kasi ho?" },
+  { id: 2, title: "26th July:- The Day We First Talked", paragraph: "A chance encounter that changed everything. The universe conspired to bring us together in the most unexpected way." },
+  { id: 3, title: "30th July:- First Conversation", paragraph: "You asked, 'Wo tumhi the na jo App ka puch rahe the' Sentence that changed the future. We then talked regarding the app feedback until the teacher Appeared." },
+  { id: 4, title: "13th August:- First Walk", paragraph: "I was extremely nervous. I waited until you arrived. I asked. You agreed. And somehow… we started walking together,By the way Do you remember when Maharashtra was formed 🤔??"},
+  { id: 5, title: "14th August:- First Temple Visit and Eating together.",
 paragraph: "The Day that I still think was a Dream (Or Dream come true) Wase 2.5 Rupees toh chaiye hi, wo bhi in cash." },
-  { id: 5, title: "22nd August", paragraph: "First time you came and said something first, 'Hi' and I asked for the time 7:46 pm, Sath me Juice 🥤 and You came to know that you bag is not fully waterproof 😂" },
-  { id: 6, title: "30th August", paragraph: "Luck was really with us I guess.Sath Momos Kha paye,Tb hi tumne bataya tha,The Pure Nature " },
-  { id: 7, title: "5th September ", paragraph: "Sath me Pandal dekhe, Sath me Corn khaye." },
-  { id: 8, title: "Mid September when we weren't able to meet", paragraph: "Days where we tried, we waited, we wanted to meet but luck didn't favour, But for a reason:- Tough us importance of each other. " },
-  { id: 9, title: "28th September", paragraph: "We both tried it worked we meet, we Walked, we Talked, We talked while walking, Mandir gaye, Spring Roll kahye, And hum apni bandi ko uss din se Tum bulaye. wase kabhi maan kiya toh Aap bolenge" },
-  { id: 10, title: "10th October", paragraph: "Still Calling it A LUCKY Day. When I stood on my quote. 'If you don't like your destiny don't accept it, Instead have the courage to change it the way you want it to be'." },
-  { id: 11, title: "11th October", paragraph: "Someone Said Party in the class, Someone Ordered for the party, and see that 2 someone, became Everyone of eachother. And The 1st photo of us together 😅." },
-  { id: 12, title: "14th October:-A GoodBye", paragraph: "Uss din 8:30 tk tumhe kisis ne khane ke liye call nahi kiye, You were free but Mera toh literally train choot raha tha. " },
-  { id: 13, title: "During Diwali break", paragraph: "I thought of the Nazm 'Woh' I didn't knew who that was, But I guess I wished that to be You" },
-  { id: 14, title: "30th October:- Our 1st call ", paragraph: `The Day I meet most Baatuni Bacha 🫵,I don't know why but somehow I liked it.🫣` },
-  { id: 15, title: "31st October:- Your 1st message", paragraph: "Point from where Love magic was mistaken as Black Magic, ku ki hum soche hi rahe the ki tum whatsapp pe nhi ho, Can't message you, Aur hum ab call toh kya hi karenge." },
+  { id: 6, title: "22nd August", paragraph: "First time you came and said something first, 'Hi' and I asked for the time 7:46 pm, Sath me Juice 🥤 and You came to know that you bag is not fully waterproof 😂" },
+  { id: 7, title: "30th August", paragraph: "Luck was really with us I guess.Sath Momos Kha paye,Tb hi tumne bataya tha,The Pure Nature " },
+  { id: 8, title: "5th September ", paragraph: "Sath me Pandal dekhe, Sath me Corn khaye." },
+  { id: 9, title: "Mid September when we weren't able to meet", paragraph: "Days where we tried, we waited, we wanted to meet but luck didn't favour, But for a reason:- Tough us importance of each other. " },
+  { id: 10, title: "28th September", paragraph: "We both tried it worked we meet, we Walked, we Talked, We talked while walking, Mandir gaye, Spring Roll kahye, And hum apni bandi ko uss din se Tum bulaye. wase kabhi maan kiya toh Aap bolenge" },
+  { id: 11, title: "10th October", paragraph: "Still Calling it A LUCKY Day. When I stood on my quote. 'If you don't like your destiny don't accept it, Instead have the courage to change it the way you want it to be'." },
+  { id: 12, title: "11th October", paragraph: "Someone Said Party in the class, Someone Ordered for the party, and see that 2 someone, became Everyone of eachother. And The 1st photo of us together 😅." },
+  { id: 13, title: "14th October:-A GoodBye", paragraph: "Uss din 8:30 tk tumhe kisis ne khane ke liye call nahi kiye, You were free but Mera toh literally train choot raha tha. " },
+  { id: 14, title: "During Diwali break", paragraph: "I thought of the Nazm 'Woh' I didn't knew who that was, But I guess I wished that to be You" },
+  { id: 15, title: "30th October:- Our 1st call ", paragraph: `The Day I meet most Baatuni Bacha 🫵,I don't know why but somehow I liked it.🫣` },
+  { id: 16, title: "31st October:- Your 1st message", paragraph: "Point from where Love magic was mistaken as Black Magic, ku ki hum soche hi rahe the ki tum whatsapp pe nhi ho, Can't message you, Aur hum ab call toh kya hi karenge." },
 
-  { id: 16, title: "November 1-20", paragraph: `We talked (80:20),Aur You talk, I lesson, I sleep you still talk.Baat toh that Mere ko Subha uthane ka 🫣
+  { id: 17, title: "November 1-20", paragraph: `We talked (80:20),Aur You talk, I lesson, I sleep you still talk.Baat toh that Mere ko Subha uthane ka 🫣
 koi toh subha tk utha deti thi 😉
 But jokes apart, Apan itna baat Kiya, Jitna hum pure saal me kisis se uthna baat nhi kiyehonge.
 We Knew eachother properly. ` },
-  { id: 17, title: "19-21st November", paragraph: `19:- I meet the most beautiful delivery Girl ever,
+  { id: 18, title: "19-21st November", paragraph: `19:- I meet the most beautiful delivery Girl ever,
 20:- I slept, Aayu Woke up, Aayu Sad, someone calmed him.( and said Jo hota hai Ache ke liye hota hai)
 21:- Bahar nikle, Ek sundar si ladki aai, Khane ka di aur Chali gai.Aayu chor Aayush bhi Khush hua,  `},
-  { id: 18, title: "30th November:- Our 1st Photo Together", paragraph: `First time we played together.
+  { id: 19, title: "30th November:- Our 1st Photo Together", paragraph: `First time we played together.
 Your friends out, You free, I asked you agreed
 I gave you the peacock feathers, Flower.
 I was Hungry till evening, just to taste the Noodles made for me with love,Aur Noodles Ache the, Hum Tereko cherne ke liye aase kehte hai.` },
 
-{ id: 19, title: "1st December", paragraph: "We again planned, you brought your laptop, Sath Photo dekhe, ( Karib se tumhe dekhe 🫣) ." },
+{ id: 20, title: "1st December", paragraph: "We again planned, you brought your laptop, Sath Photo dekhe, ( Karib se tumhe dekhe 🫣) ." },
 
-{ id: 20, title: "3st December", paragraph: "Mera Bacha Mandir nhi aaya, Bacha ka tabiyat bhi theek nhi lg raha tha, Bacha sad tha, So I Gave my Favourite flowes to My Forever Favourite Person (My JULIE) " },
+{ id: 21, title: "3st December", paragraph: "Mera Bacha Mandir nhi aaya, Bacha ka tabiyat bhi theek nhi lg raha tha, Bacha sad tha, So I Gave my Favourite flowes to My Forever Favourite Person (My JULIE) " },
 
-{ id: 21, title: "10-11th December", paragraph: `Hum nashe me the, But still I remembered you told on 9th ki 10th se hr roz milenge, thore der kiliye bhi but milenge.
+{ id: 22, title: "10-11th December", paragraph: `Hum nashe me the, But still I remembered you told on 9th ki 10th se hr roz milenge, thore der kiliye bhi but milenge.
 10:- we Planned to go Radha Krishna Mandir, but Went to Park instead. 
 11:- I guess You choosed this number ` },
 
-{ id: 22, title: "12th & 17th December", paragraph: `We finally Went to Radha Krishna Mandir.
+{ id: 23, title: "12th & 17th December", paragraph: `We finally Went to Radha Krishna Mandir.
 12:- 1st time Sath me Mess me khaye the. 
 17:- I wrote something...... Then Below Partner I wrote (A) and Obviously By (A) I mean Aayush, Haaiii na 😏`},
 
-{ id: 23, title: "13th December", paragraph: "The Day You Trusted me." },
+{ id: 24, title: "13th December", paragraph: "The Day You Trusted me." },
 
-{ id: 24, title: "14th December", paragraph: "We Went to The Park for 2nd time , We ate the Dinner together at the Mess for 2nd time ☺️" },
+{ id: 25, title: "14th December", paragraph: "We Went to The Park for 2nd time , We ate the Dinner together at the Mess for 2nd time ☺️" },
 
-{ id: 25, title: "16th December", paragraph: `Sath me Potato twisters khaye, Chowmin, Pani Puri, Ice Cream 😌😉.
+{ id: 26, title: "16th December", paragraph: `Sath me Potato twisters khaye, Chowmin, Pani Puri, Ice Cream 😌😉.
 Ha toh tum yaha tk read ki ho, Agar kuch km ya incomplete lage toh hum call pe hi hai, puch lo.` },
 
-{ id: 26, title: "18th December:- We didn’t knew the future. But we chose each other", paragraph: `Our 1st Kiss:- 6:26 pm 
+{ id: 27, title: "18th December:- We didn’t knew the future. But we chose each other", paragraph: `Our 1st Kiss:- 6:26 pm 
 Confession:- 7:26 pm
 Aur Fir kya hi Bole 18th ke bare me🫣,
 All that Fun, Late night RUN 🏃‍♂️🤝🏃‍♀️
 Fir to save my Bladder Run 🏃‍♂️🏃‍♂️💨` },
 
-{ id: 27, title: "19th December", paragraph: `A full Day With MY Bandi.
+{ id: 28, title: "19th December", paragraph: `A full Day With MY Bandi.
 Scooty Ride,Ganesh ji temple, Kachori,Mr Bean,And then Those Moments 🤌. `},
 
-{ id: 28, title: "20th December:-It was never easy to leave", paragraph: `Morning Walk, Chai,Park,Mandir,30 nov ko apni friend ko jhulaye the, 20 Dec ko Abhi Bandi ko. 
+{ id: 29, title: "20th December:-It was never easy to leave", paragraph: `Morning Walk, Chai,Park,Mandir,30 nov ko apni friend ko jhulaye the, 20 Dec ko Abhi Bandi ko. 
 Kala Dhaga, Necklace, Basketball court, and then a good bye.
 but came to know That Good bye wasn't finally, Bcz someone prepared something very Special fro me.
 I don't know how to end, Hum call pe hi hai, Pucho lo`},
